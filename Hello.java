341dbab2dd58b5f3929be8353e17fdd97e169b93
Kaif Kazmi <sayedkaif784@gmail.com>
2025-11-02 19:15:48 +0530

    updated java file

--- Hello.java
+++ Hello.java
@@ -3,6 +3,15 @@ package GithubWorkshop;
 public class Hello {
     public static void main(String[] args) {
         System.out.println("hello world");
+
+
+        int a = 5;
+        int b = 19;
+
+
+        for (int i=0;i<5;i++){
+            System.out.print(i);
+        }
     }
 }
 
